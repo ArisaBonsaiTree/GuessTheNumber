234b3d4d055c2132b5f1b8c6443a0eecca494d90
ArisaBonsaiTree <angelevarela@hotmail.com>
2023-03-16 23:05:53 -0700

    Refactored and tested!

--- src/main/java/com/av/guessTheNumber/service/ServiceLayer.java
+++ src/main/java/com/av/guessTheNumber/service/ServiceLayer.java
@@ -10,11 +10,12 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.*;
-import java.util.stream.Collectors;
+
 
 @Service
 public class ServiceLayer {
-    private static String REDACTED = "****";
+    private final String DELIMITER = ":";
+    private final String REDACTED = "****";
 
     @Autowired
     GameDao gameDao;
@@ -41,7 +42,6 @@ public class ServiceLayer {
         return games;
     }
 
-
     public Round makeGuess(Guess guess) {
         Game game = gameDao.getGameById(guess.getGameId());
         if(game == null || guess.getUserGuess() == null) return null;
@@ -49,59 +49,22 @@ public class ServiceLayer {
         String answer = game.getGeneratedNumber();
         String userGuess = guess.getUserGuess();
 
-        Map<String, Integer> answerKey = new HashMap<>();
+        Map<String, Integer> answerKey = getAnswerKey(answer);
 
-        for (int i = 0; i < answer.length(); i++) {
-            String digit = answer.substring(i, i + 1);
-            answerKey.put(digit, answerKey.getOrDefault(digit, 0) + 1);
-        }
+        String[] result = getResults(userGuess, answer, answerKey).split(DELIMITER);
 
-        int exact = 0;
-        int partial = 0;
+        int exact = Integer.parseInt(result[0]);
+        int partial = Integer.parseInt(result[1]);
 
-        Map<String, Integer> remainingDigits = new HashMap<>(answerKey);
-
-        for (int i = 0; i < userGuess.length(); i++) {
-            String digit = userGuess.substring(i, i + 1);
-
-            if (answerKey.containsKey(digit)) {
-
-                if (answer.substring(i, i + 1).equals(digit)) {
-                    exact++;
-                    remainingDigits.put(digit, remainingDigits.get(digit) - 1);
-                } else if (remainingDigits.getOrDefault(digit, 0) > 0) {
-                    partial++;
-                    remainingDigits.put(digit, remainingDigits.get(digit) - 1);
-                }
-            }
-        }
-        // Round Id is unknwon
-        Round round = new Round();
-
-        round.setGameId(game.getGameId());
-        round.setUserGuess(userGuess);
-        round.setPartialCorrect(partial);
-        round.setExactCorrect(exact);
-        round.setTimeStamp(LocalDateTime.now());
+        // Round Id is unknown
+        Round round = createRound(game, userGuess, exact, partial);
 
         if(exact == game.getGeneratedNumber().length()){
             game.setInProgress(false);
             gameDao.updateGame(game);
         }
-
+        
         return roundDao.addRound(round);
-    }
-
-
-
-
-    private String generateRandomNumber(){
-        List<Integer> digits = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            digits.add(i);
-        }
-        Collections.shuffle(digits);
-        return "" + digits.get(0) + digits.get(1) + digits.get(2) + digits.get(3);
     }
 
     public Game getGameById(int gameId) {
@@ -117,4 +80,56 @@ public class ServiceLayer {
     public List<Round> getRoundsForGame(int gameId) {
         return roundDao.getRoundsByGameId(gameId);
     }
+
+    private String getResults(String userGuess, String answer, Map<String, Integer> answerKey){
+        int exact = 0;
+        int partial = 0;
+
+        for (int i = 0; i < userGuess.length(); i++) {
+            String digit = userGuess.substring(i, i + 1);
+
+            if (answerKey.containsKey(digit)) {
+
+                if (answer.substring(i, i + 1).equals(digit)) {
+                    exact++;
+                } else {
+                    partial++;
+                }
+            }
+        }
+
+        return exact + "" + partial;
+    }
+
+    private Round createRound(Game game, String userGuess, int exact, int partial) {
+        Round round = new Round();
+
+        round.setGameId(game.getGameId());
+        round.setUserGuess(userGuess);
+        round.setPartialCorrect(partial);
+        round.setExactCorrect(exact);
+        round.setTimeStamp(LocalDateTime.now());
+
+        return round;
+    }
+
+    private Map<String, Integer> getAnswerKey(String answer) {
+        Map<String, Integer> answerKey = new HashMap<>();
+
+        for (int i = 0; i < answer.length(); i++) {
+            String digit = answer.substring(i, i + 1);
+            answerKey.put(digit, answerKey.getOrDefault(digit, 0) + 1);
+        }
+        return answerKey;
+    }
+
+    private String generateRandomNumber(){
+        List<Integer> digits = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            digits.add(i);
+        }
+        Collections.shuffle(digits);
+        return "" + digits.get(0) + digits.get(1) + digits.get(2) + digits.get(3);
+    }
+
 }
